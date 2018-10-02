e5f8796a85f88af907b64be0abf4109bf2caf872
Gravestorm <gravestormy@gmail.com>
2018-10-02 18:05:09 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -124,12 +124,12 @@ client.on('ready', () => {
     if (!nconf.get('STREAMING_ROLE')) {
       return;
     } else if (nconf.get('STREAMING_GAME')) {
-      client.guilds.find(guild => guild.id === nconf.get('SERVER')).fetchMembers()
       setInterval(() => {
         R.forEach(member => {
+          client.guilds.find(guild => guild.id === nconf.get('SERVER')).fetchMember(member.id);
           if (member.roles.has(nconf.get('STREAMING_ROLE')) !== true && member.roles.has('91854670766559232') === true) {
             // member.addRole(nconf.get('STREAMING_ROLE'));
-            // console.log('1' + member.displayName + member.presence.game);
+            console.log('1' + member.displayName + member.presence.game);
             return;
           } else if (member.roles.has(nconf.get('STREAMING_ROLE')) === true) {
             // member.removeRole(nconf.get('STREAMING_ROLE'));
